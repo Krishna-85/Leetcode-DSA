72462a19e714335d7816b9099a6cb80318645316
Dev Krishna <vaibhavchauhan658@gmail.com>
2025-02-06 12:22:05 +0530

    Time: 7 ms (9.95%), Space: 50.7 MB (44.4%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -6,7 +6,7 @@ var checkIfExist = function(arr) {
    arr.sort((a,b)=>a-b)
    for(let i =0; i<arr.length; i++){
     for(let j=0; j<arr.length; j++){
-        if(i!=j && 2*arr[i]==arr[j])  return true
+        if(i!=j &&2*arr[i]==arr[j])  return true
     }
    } 
    return false
