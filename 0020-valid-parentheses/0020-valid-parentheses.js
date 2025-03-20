67fcf7aad4cc57e8e24db7fb2e8972b8b81eb7f1
Dev Krishna <vaibhavchauhan658@gmail.com>
2025-03-20 12:40:58 +0530

    Time: 5 ms (32.02%), Space: 56.9 MB (13.75%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    for(let i=0; i<s.length; i++){
+        let ch = s.charAt(i);
+        if(ch == '(' || ch == '[' || ch == '{'){
+            stack.push(ch);
+            continue;
+        }
+        if(stack.length ===0) return false
+         if(ch == ')'){
+            if(stack[stack.length-1]==='(') stack.pop()
+            else return false
+        }
+        if(ch == ']'){
+            if(stack[stack.length-1]==='[') stack.pop()
+            else return false
+        }
+         if(ch == '}'){
+            if(stack[stack.length-1]==='{') stack.pop()
+            else return false
+        }
+       
+    }
+if(stack.length !==0) return false;
+return true
+
+};
